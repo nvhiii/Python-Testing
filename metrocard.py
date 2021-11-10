4ce9981da6507d9427c0ba864b9287263b129403
nvhiii <nahikhan16@gmail.com>
2021-11-09 19:08:40 -0500

    actually added something in the python file

---
+++ metrocard.py
@@ -0,0 +1,10 @@
+#A subway fare is $2.75 per trip.
+#A 30-day unlimited MetroCard costs $127.
+#Write a Python script in metrocard.py that calculates
+#and prints the minimum number trips you need to break even on purchasing a 30-day MetroCard.
+
+trip = 2.75
+ThirtyUnlimited = 127.0
+
+breakEven = ThirtyUnlimited/trip
+print(breakEven)
